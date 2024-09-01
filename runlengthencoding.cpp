bb3457d685027df7da2a9ddba0b4eb0451d2e70c
Devon Hobbs <hobbsdevonk@gmail.com>
2024-09-01 10:40:51 -0400

    added functions & prototypes

--- runlengthencoding.cpp
+++ runlengthencoding.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
+
+std::string encode(std::string);
+std::string decode(std::string);
+
 
 int main(){
 
     std::cout << "test" << std::endl;
 
+}
+
+std::string encode(std::string input){
+    if (input.empty()) return "Input string is empty, please try again";
+    std::string encoded;
+    int count = 0;
+    char currentChar = input[0];
+
+    return encoded;
+}
+
+std::string decode(std::string encoded){
+    std::string decoded;
+
+    return decoded;
 }
