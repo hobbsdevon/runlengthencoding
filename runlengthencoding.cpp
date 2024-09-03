d70e9b13591d38bc4b7bbbe9d029b434ea359586
Devon Hobbs <hobbsdevonk@gmail.com>
2024-09-02 20:28:13 -0500

    encode() functional

--- runlengthencoding.cpp
+++ runlengthencoding.cpp
@@ -7,16 +7,27 @@ std::string decode(std::string);
 
 int main(){
 
-    std::cout << "test" << std::endl;
+    std::cout << encode("aaabbccccccccccddddddefg") << std::endl;
 
 }
 
 std::string encode(std::string input){
     if (input.empty()) return "Input string is empty, please try again";
     std::string encoded;
-    int count = 0;
-    char currentChar = input[0];
-
+    int count = 1;
+    for(int i = 0; i < input.length(); i++){
+        char currentChar = input[i];
+        char nextChar = input[i + 1];
+        //std::cout << currentChar << " " << nextChar << std::endl;
+        if(currentChar == nextChar){
+            count++;
+        } else {
+            encoded += currentChar;
+            encoded += std::to_string(count);
+            //std::cout << count << std::endl;
+            count = 1;
+        }
+    }
     return encoded;
 }
 
